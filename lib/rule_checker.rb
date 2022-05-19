ac32057637329307dd80216fde6a1e87243c7bd3
Javi <jav.olivares3@gmail.com>
2022-05-19 13:33:26 -0500

    #move_checker now prevents own piece from moving into players own pieces.

--- lib/rule_checker.rb
+++ lib/rule_checker.rb
@@ -1,14 +1,24 @@
 require_relative 'pieces'
 include Pieces
 
-def move_checker(selected, opponent_set)
+def move_checker(selected, player_set, opponent_set)
   case selected.type
    #Ensures king does not move into check 
   when 'king'
     arr = []
-    opponent_set.each do |piece|
-      common = selected.children & piece.children
+    player_set.each do |piece|
+      p "#{selected.children} vs #{piece.position}"
+      common = selected.children & [piece.position]
       arr.push(common) unless common.empty?
+    end
+    p arr
+    opponent_set.each do |piece|
+      if piece.type == 'pawn'
+        #push only pawns 2 capture moves
+      else
+        common = selected.children & piece.children
+        arr.push(common) unless common.empty?
+      end
     end
     arr = arr.flatten(1).uniq unless arr.empty?
     selected.children - arr 
